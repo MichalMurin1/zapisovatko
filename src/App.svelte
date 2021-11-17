080955d4d564c0d2cc56b25278f04d7e822fdef5
michal.murin <michal.murin.mail@gmail.com>
2021-11-17 19:04:30 +0100

    basic progress

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import ActDay from './components/ActDay.svelte';
+	export let item: string;
+	export let tasks: [];
+	
+</script>
+
+<main class="bg-gray-800 text-white w-screen h-screen flex text-gray-300">
+	<section class="flex mt-6 max-w-screen-xl px-8 w-full">
+		<div class="w-1/4">
+			<h1 class="text-center text-3xl font-bold">Zapisovatko</h1>
+		</div>
+		<div class="w-3/4 pt-16">
+			<ActDay></ActDay>
+
+			<div class="pb-4 border-b border-gray-900">
+				<input type="text" class="p-3 w-full bg-gray-900 placeholder-gray-600" placeholder="Add a task...">
+			</div>
+			<div class="p-4 w-full">
+				<h3 class="text-xl text-bold mb-2">Your todays tasks: </h3>
+				<ul id="js-items" class="js-items">
+					<li class="p-3 border-b border-gray-700">
+						<label class="cursor-pointer">
+							<input type="checkbox" class="mr-2" value="1" name="task-1">
+							<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, quae?</span>
+						</label>
+					</li>
+					<li class="p-3 border-b border-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, quae?</li>
+					<li class="p-3 border-b border-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, quae?</li>
+					<li class="p-3 border-b border-gray-700">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro, quae?</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+</main>
---
+++ src/components/ActDay.svelte
@@ -0,0 +1,40 @@
+<script>
+    export const date = new Date();
+    
+    const weekday = new Array(7);
+	weekday[0] = "Sunday";
+	weekday[1] = "Monday";
+	weekday[2] = "Tuesday";
+	weekday[3] = "Wednesday";
+	weekday[4] = "Thursday";
+	weekday[5] = "Friday";
+	weekday[6] = "Saturday";
+
+	const months = new Array(12);
+	months[0] = "Jan";
+	months[1] = "Feb";
+	months[2] = "Mar";
+	months[3] = "Apr";
+	months[4] = "May";
+	months[5] = "Jun";
+	months[6] = "Jul";
+	months[7] = "Aug";
+	months[8] = "Sep";
+	months[9] = "Oct";
+	months[10] = "Nov";
+	months[11] = "Dec";
+
+	let day;
+	day = weekday[date.getDay()];
+
+</script>
+
+<div class="mx-auto max-w-xs flex mb-10 items-center">
+    <div>-></div>
+    <div class="text-center px-4">
+        <p class="text-5xl mb-3">{day}</p>
+        <p class="text-xl">{months[date.getMonth()]+' '+ date.getDate()}, {date.getFullYear()}</p>
+        <p class="text-xl">Score: 0/7</p>
+    </div>
+    <div>-></div>
+</div>
